53cf1aaf7b2e170312b7b29e0f8ceaeb1900cc64
lage356 <fernando.lage356@gmail.com>
2024-01-29 08:02:01 -0800

    Added JS displaytime functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-
 $(function () {
+  var timeDisplayEl = $("#currentDay");
+  var currentHour = dayjs().format("HH:mm:ss a");
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -18,4 +19,11 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  function displayTime() {
+    var rightNow = dayjs().format("dddd MMMM DD, YYYY ");
+    timeDisplayEl.text(rightNow);
+  }
+  
+
+  displayTime();
 });
