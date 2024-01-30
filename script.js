73c1e5747c5b590e790393ae6bb93007e5d3cc69
lage356 <fernando.lage356@gmail.com>
2024-01-29 16:34:54 -0800

    Update the event listener in js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-$(function () {
+$(document).ready(function() { 
   var timeDisplayEl = $("#currentDay");
   var currentHour = dayjs().format("HH:mm:ss a");
   var newID = 8;
-  var buttonList = $('.btn');
+  var clickID =8;
+  var buttonList = $(".btn");
   var hour;
   var endhour;
 
@@ -14,65 +15,54 @@ $(function () {
   // useful when saving the description in local storage?
   //
 
-  buttonList.on('click', function (event){
-    console.log(event);
+  buttonList.on("click", function (event) {
+    
+    var value = $(this).siblings(".description").val();
+    var time = $(this).parent().attr('id');
+
+    console.log(value);
+    console.log(time);
+
+    clickID = clickID +1;
+    var scheduleEl = $('#hour-' + clickID);
+    var getValue = scheduleEl.children().eq(1).val();
+  
+
   });
 
-  
   // TODO: Add code to apply the past, present, or future class to each time
 
   function renderColors() {
+    $(".evento").each(function (i, event) {
+      var hour = $(this).data("hour");
+      var endhour = $(this).data("endhour");
 
-
-    $('.evento').each(function(i, event){
-      
-      var hour = $(this).data('hour');
-      var endhour = $(this).data('endhour');
-    
       newID = newID + 1;
-      var newElement =$("#hour-" + newID);
+      var newElement = $("#hour-" + newID);
 
-      if(currentHour>hour && currentHour < endhour){
+      if (currentHour > hour && currentHour < endhour) {
         newElement.addClass("present");
-      }else if (currentHour>hour){
+      } else if (currentHour > hour) {
         newElement.addClass("past");
-      }else if (currentHour < hour){
-        newElement.addClass("future")
+      } else if (currentHour < hour) {
+        newElement.addClass("future");
       }
     });
-  //  events.forEach((event) => {
-
-
-      newID = newID + 1;
-      var newElement =$("#hour-" + newID);
-
-      if(currentHour>hour && currentHour <endhour){
-        newElement.addClass("present");
-      }else if (currentHour>hour){
-        newElement.addClass("past");
-      }else if (currentHour < hour){
-        newElement.addClass("future")
-      }
-  //   });
-    
   }
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
 
-  function saveScheduletoStorage(){
-    
-    var data = $('#hour-9').children('textarea').eq(0);
+  function saveScheduletoStorage() {
 
-    console.log(data);
   }
+
   // TODO: Add code to display the current date in the header of the page.
   function displayTime() {
     var rightNow = dayjs().format("dddd MMMM DD, YYYY ");
     timeDisplayEl.text(rightNow);
   }
-  
 
   displayTime();
   renderColors();
