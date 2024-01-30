03cdf7e9008246690a8ecb6e45a82eaae77d6552
lage356 <fernando.lage356@gmail.com>
2024-01-29 21:47:41 -0800

    update js and html file

--- script.js
+++ script.js
@@ -1,62 +1,44 @@
-$(document).ready(function() { 
+$(document).ready(function () {
   var timeDisplayEl = $("#currentDay");
-  var currentHour = dayjs().format("HH:mm:ss a");
-  var newID = 8;
+  var currentHour = dayjs().hour();
   var buttonList = $(".btn");
-  var scheduleEvents = [];
+
 
   // TODO: Add a listener for click events on the save button. This code should
 
-
   buttonList.on("click", function (event) {
-    
     var value = $(this).siblings(".description").val();
-    var time = $(this).parent().attr('id');
+    var time = $(this).parent().attr("id");
 
-    var scheduleObj = {
-      hora: time,
-      schedule: value
-    }
-
-    scheduleEvents.push(scheduleObj);
-    localStorage.setItem("Events Scheduled", JSON.stringify(scheduleEvents));
-
+    localStorage.setItem(time, value);
   });
 
   // TODO: Add code to apply the past, present, or future class to each time
 
-  function renderColors() {
-    $(".evento").each(function (i, event) {
-      var hour = $(this).data("hour");
-      var endhour = $(this).data("endhour");
+  $(".evento").each(function () {
+    var hour = parseInt($(this).attr("id").split("-")[1]);
+    console.log(hour);
 
-      newID = newID + 1;
-      var newElement = $("#hour-" + newID);
-
-      if (currentHour > hour && currentHour < endhour) {
-        newElement.addClass("present");
-      } else if (currentHour > hour) {
-        newElement.addClass("past");
-      } else if (currentHour < hour) {
-        newElement.addClass("future");
-      }
-    });
-  }
+    if (currentHour === hour) {
+      $(this).addClass("present");
+    } else if (currentHour > hour) {
+      $(this).addClass("past");
+    } else {
+      $(this).addClass("future");
+    }
+  });
+  
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-
-  function saveScheduletoStorage() {
-
+  
+  for (var i = 9; i < 18; i++) {
+    $("#hour-" + i + " textarea").val(localStorage.getItem("hour-" + i));
   }
 
   // TODO: Add code to display the current date in the header of the page.
-  function displayTime() {
-    var rightNow = dayjs().format("dddd MMMM DD, YYYY ");
-    timeDisplayEl.text(rightNow);
-  }
 
-  displayTime();
-  renderColors();
+  var rightNow = dayjs().format("dddd MMMM DD, YYYY ");
+  timeDisplayEl.text(rightNow);
+
 });
